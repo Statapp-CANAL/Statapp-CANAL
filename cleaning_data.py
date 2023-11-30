e1d6d6b1b84cc49ad3e6bc9e7160571ec2bf8547
MaximeCoppa <maxime.coppa@gmail.com>
2023-11-30 15:39:37 +0100

    Data

---
+++ cleaning_data.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np
+
+
+def file_to_dataframe(filenames,st): 
+    """
+    Initialize the DataFrame from a filenames 
+    Parameters: 
+    -----------
+    filenames: str, the name of the file
+    st: str, helps to delimites the columns of the datas because "Correspondances_promo
+    are delimited with ';' while the other are delimited with ','.
+    """
+    datas = pd.read_csv(filenames,delimiter=st)
+    return datas
+
+
+def clean_dates(df):
+    """
+    Clean the dates of the files with dates such as "2022-11-14T00:00:00Z" and change it into "2022-11-14"
+    -----------
+    df: dataframe, the dataframe that we use
+    
+    """
+    for colu in df.columns :
+        if df[colu].dtype == object: 
+            df[colu] = df[colu].str.replace('T00:00:00Z', '')
+    return(df)
+
+def change_date(st):
+    """
+    Change a date as "28/04/2003 00:00:00.000" into one more precise "2003-04-28"
+    -----------
+    st: str, the date to change
+
+    """ 
+    if type(st) != str or len(st) < 10 : 
+        return(st)
+    else :
+        date = st[:10].split("/")
+        return(date[2]+"-"+date[1]+"-"+date[0])
+    
+def change_dates_all(df,cols):
+    """
+    Change the dates of some columns of a dataFrame using the function change_date
+    -----------
+    df: dataframe, the DataFrame on which we are working
+    cols: str list, a list of columns name that we want to change
+
+    """ 
+    for colu in cols : 
+        df[colu] = df[colu].apply(change_date)
+    return df
+
+def save_to_csv_file(df,filename):
+    """
+    Save a dataframe on a .csv file at filename
+    ----------
+    df: dataframe, the DataFrame that you want to save
+    filename: str, the place where you want to save your data frame
+    """
+    df.to_csv(filename, index=False)
+    return True
+
+
+
+
+df = file_to_dataframe("/Users/maximecoppa/Desktop/Statapp/Datas/Correspondances_Promos_2.csv",";")
+df_Correspondances_Promos = change_dates_all(df,['DEBVAL', 'FINVAL', 'DEBABOMIN', 'DEBABOMAX'])
+save_to_csv_file(df_Correspondances_Promos,"/Users/maximecoppa/Desktop/Statapp/Datas_clean/df_Correspondances_Promos.csv")
+save_to_csv_file(df_Correspondances_Promos,"/Users/maximecoppa/Desktop/Statapp/Datas_head/df_Correspondances_Promos.csv")
+df = file_to_dataframe("/Users/maximecoppa/Desktop/Statapp/Datas/Données_Promos_2021.csv",",")
+df_Données_Promos_2021 = clean_dates(df)
+save_to_csv_file(df_Données_Promos_2021,"/Users/maximecoppa/Desktop/Statapp/Datas_clean/df_Données_Promos_2021.csv")
+save_to_csv_file(df_Données_Promos_2021.head(),"/Users/maximecoppa/Desktop/Statapp/Datas_head/df_Données_Promos_2021_head.csv")
+df = file_to_dataframe("/Users/maximecoppa/Desktop/Statapp/Datas/Données_Reabos_2021.csv",",")
+df_Données_Reabos_2021 = clean_dates(df)
+save_to_csv_file(df_Données_Reabos_2021.head(),"/Users/maximecoppa/Desktop/Statapp/Datas_head/df_Données_Reabos_2021_head.csv")
+
+
---
+++ join_data.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+
+def file_to_dataframe(filename,st): 
+    """
+    Initialize the DataFrame from a filenames 
+    Parameters: 
+    -----------
+    filenames: str, the name of the file
+    st: str, helps to delimites the columns of the datas because "Correspondances_promo
+    are delimited with ';' while the other are delimited with ','.
+    """
+    datas = pd.read_csv(filename,delimiter=st)
+    return datas
+
+def join_dataFrames(df1,df2,cond):
+    """
+    Join two DataFrames on the conditions cond
+    Parameters: 
+    -----------
+    df1,df2: DataFrames
+    cond: str list of the colomuns on which we want to join 
+    """
+    return pd.merge(df1, df2, on=cond, how='inner')
+
+def calculate_nb_promos(df):
+    return(df.groupby('CPROMO')['ID_ABONNE'].count())
+
+df_Correspondances_Promos = file_to_dataframe("/Users/maximecoppa/Desktop/Statapp/Datas_head/df_Correspondances_Promos.csv",",")
+df_Données_Promos_2021 = file_to_dataframe("/Users/maximecoppa/Desktop/Statapp/Datas_clean/df_Données_Promos_2021.csv",",")
+df_Données_Reabos_2021 = file_to_dataframe("/Users/maximecoppa/Desktop/Statapp/Datas_clean/df_Données_Reabos_2021.csv",",")
+
+df_join = join_dataFrames(df_Données_Promos_2021,df_Données_Reabos_2021,['ID_ABONNE','DATE_ACTE_REEL'])
+n,_ = df_join.shape
+df_nb_promos = calculate_nb_promos(df_join)
+print((df_nb_promos).sort_values(ascending=False))
+print(df_Données_Promos_2021.shape)
+print(df_Données_Reabos_2021.shape)
