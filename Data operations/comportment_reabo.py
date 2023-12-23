df628518c75a40840cb48272143695207dad4279
Antoine Gilson <125046174+AntoineGilsonGH@users.noreply.github.com>
2023-12-23 15:22:34 +0100

    Add files via upload

---
+++ Data operations/comportment_reabo.py
@@ -0,0 +1,155 @@
+import pandas as pd
+import math 
+
+from join_data import join_dataFrames
+
+
+#Some functions to manipulate the dates 
+
+def str_to_date(df,col_name):
+    """
+    This function transforms a column of a DataFrame 
+    to the type date
+    """
+    try:
+        df[col_name] = pd.to_datetime(df[col_name])
+    except ValueError:
+        print("This column is not a date")
+        return ValueError
+    return df
+
+def df_mois_annee(df,name_col) :
+    """
+    This function creates two a columns "MONTH" and "YEAR" of a DataFrame 
+    which correspond to the "MONTH" and the "YEAR" of name_col
+    """
+    try: 
+        df[name_col] = pd.to_datetime(df[name_col])
+        df['MONTH'] = df[name_col].dt.month
+        df['YEAR'] = df[name_col].dt.year
+
+    except ValueError:
+        print("This column is not a date")
+        return ValueError
+    
+    return df
+
+
+def time_reabo_columns(df,end_abo,date_reabo):
+    """
+    This function creates a new column in the DataFrame which represents the delay between the end 
+    of the last abo and  the act of reabo
+    """
+    df = str_to_date(df,date_reabo)
+    df = str_to_date(df,end_abo)
+    df["DELAI_REABO"] = (df[date_reabo] - df[end_abo]).dt.days
+    return df
+
+#1) 
+#In this part we write some functions to create the new column odd
+
+def df_filter_condition(df,str_name, str_cond):
+    """
+    This function filters the elemenent of the columns str_name with the condition ==
+    str_cond
+    """
+
+    df = df[df[str_name] == str_cond]
+    return df
+
+
+def df_filter_begin_name(df, str_name, str_filter):
+    """
+    This function filters the elemenent of the columns str_name which begins with 
+    str_filter
+    """
+    return df[df[str_name].str.startswith(str_filter)]
+
+
+def apply_conditions(row):
+    """
+    This fucntion creates the elements of a new column where the promos are put
+    together in function of some conditions.
+    """
+    nb_days = 'NBJOUR_ODD'
+    carticle = 'CARTICLE_ODD'
+    if row['SG'] == 'Semaine généreuse':
+        return 'Semaine genéreuse'
+    elif row[nb_days] == 7 :
+        return 'ODD 7 jours autre que SG'
+    elif (row[carticle][:2] == 'EV') & ((row[nb_days] == 15) | (row[nb_days] == 14) ) :
+        return 'ODD 15 jours EV+'
+    elif (row[carticle][:2]  == 'TC') & ((row[nb_days] == 15) | (row[nb_days] == 14) ) :
+        return 'ODD 15 jours TC'
+    elif (row[carticle][:2] == 'EV') & (row[nb_days] == 21) :
+        return 'ODD 21 jours EV+ '
+    elif (row[carticle][:2] == 'TC') & (row[nb_days] == 21):
+        return 'ODD 21 jours EV+ '
+    else: 
+        return 'Autres'
+        
+def create_new_column(df: pd.DataFrame,function):
+    """
+    This function is used to sendback a new column using the function apply
+    and applying function to the elements 
+    """
+    return df.apply(function,axis = 1)
+
+def keep_used_odd(df_Données_Promos,df_odd,n) :
+    """
+    This function delete all the promos which are not used enough
+    """
+    df_join_odd = join_dataFrames(df_Données_Promos,df_odd,['CPROMO'])
+
+    series_count = df_join_odd.groupby(['CPROMO'])['ID_ABONNE'].count() #count the number of people who used each PROMO
+    series_count = series_count.sort_values(ascending=False) #sort the number of people
+    series_count = series_count[series_count > n ] # Keep the more used
+    series_count = series_count.rename('NOMBRE_UTILISATION') # Rename with a proper name
+
+    df_filtered_groups = series_count.reset_index() # series to Data fram
+    df_new_odd = join_dataFrames(df_filtered_groups,df_odd,['CPROMO'])
+
+    return df_new_odd
+
+#3)
+#In this part we create some functions to count and give the mean of the datas group by columns and calculating using the condition
+
+
+def count_abo_conditions(df: pd.DataFrame,columns ,cond):
+    """
+    This function count the number of elements of the condition and group by
+    the column 
+    """
+    series = df.groupby(columns)[cond].count() # Group by specified columns and count occurrences of the condition
+    df_filtered_groups = series.reset_index(name = 'NB_' + cond) #Series into DataFrames
+
+    df_filtered_groups = df_filtered_groups.sort_values(by = 'NB_' + cond, ascending=False) # Sort the DataFrame by the count column in descending order
+
+    return df_filtered_groups
+
+def mean_time_reabo(df,columns,cond):
+    """
+    This function calculate the mean of the condition and group by
+    the column 
+    """
+    series = df.groupby(columns)[cond].mean() # Group by specified columns and calculate the mean of the condition
+    df_filtered_groups = series.reset_index(name = 'MEAN_' + cond)
+
+    df_filtered_groups = df_filtered_groups.sort_values(by = 'MEAN_' + cond, ascending=False) # Sort the DataFrame by the mean column in descending order
+
+
+    return df_filtered_groups
+
+def mean_empty_col(df,column,cond):
+    """
+    This function calculate the mean of the datas of column col by dividing
+    between the datas where the columns cond is empty or not    
+    """
+
+    series = df.groupby(df[column].isna()) # Group by whether the specified column is empty or not, then calculate the mean of the condition
+    series = series[cond].mean() 
+    df_filtered_groups = series.reset_index(name = 'MEAN_IS_NA' + cond)
+
+    df_filtered_groups = df_filtered_groups.sort_values(by = 'MEAN_IS_NA' + cond, ascending=False)   # Sort the DataFrame by the mean column in descending order
+
+    return df_filtered_groups
---
+++ Data operations/test_comportment_reabo.py
@@ -0,0 +1,186 @@
+import pandas as pd
+import math 
+
+from cleaning_data import file_to_dataframe
+from cleaning_data import save_to_csv_file
+from join_data import join_dataFrames
+from join_data import join_dataFrames_outer
+
+from comportment_reabo import df_filter_condition, create_new_column, apply_conditions, keep_used_odd, count_abo_conditions, mean_empty_col, df_mois_annee, time_reabo_columns, mean_time_reabo, str_to_date
+
+#1) 
+# In this part we create a new table Correspondance_promos by putting the promo in groups 
+
+def creation_df_odd() :
+    """
+    This function creates the new df_odd on your Computer where all the ODD are presented and classified with their type
+    """
+
+    data_path = "/Users/maximecoppa/Desktop/Statapp/Datas_clean/" #where to find the datas used
+    data_path_results = "/Users/maximecoppa/Desktop/Statapp/Datas_clean/" #where to create your new file
+
+    df_Correspondances_Promos = file_to_dataframe(data_path + "df_Correspondances_Promos.csv",",") 
+
+    df_odd = df_filter_condition(df_Correspondances_Promos,'TYPE_PROMO','ODD') #we create a DataFrame with only ODD Promotion
+    df_odd['TYPE_PROMON'] = create_new_column(df_odd,apply_conditions) #we create new columns on this DataFrame of the ODD type
+
+
+    #creation df_odd_2021
+    df_Données_Promos_2021 = file_to_dataframe(data_path + "df_Données_Promos_2021.csv",",")
+
+    n = df_Données_Promos_2021.shape[0] / 10000 #number minimum of used
+    df_new_odd = keep_used_odd(df_Données_Promos_2021,df_odd,n) #creation of the new tab by keeping only the used promos
+    
+    save_to_csv_file(df_new_odd,data_path_results + "odd_2021.csv")
+
+    #creation df_odd_2022  
+    df_Données_Promos_2022 = file_to_dataframe(data_path + "df_Données_Promos_2022.csv",",")
+
+    n = df_Données_Promos_2022.shape[0] / 10000 #number minimum of used
+    df_new_odd = keep_used_odd(df_Données_Promos_2022,df_odd,n) #creation of the new tab by keeping only the used promos
+    
+    save_to_csv_file(df_new_odd,data_path_results + "odd_2022.csv")
+    
+    #creation df_odd_2023
+    df_Données_Promos_2023 = file_to_dataframe(data_path + "df_Données_Promos_2023.csv",",")
+
+    n = df_Données_Promos_2023.shape[0] / 10000 #number minimum of used
+    df_new_odd = keep_used_odd(df_Données_Promos_2023,df_odd,n) #creation of the new tab by keeping only the used promos
+    
+    save_to_csv_file(df_new_odd,data_path_results + "odd_2023.csv")
+
+    return True
+
+
+#Intermediary step we change the df_Données_Promos by adding a column 'TYPE_PROMON' for being easier to understand instead of 'CPROMO'
+
+def create_df_Données_Promos_odd():
+
+    data_path = "/Users/maximecoppa/Desktop/Statapp/Datas_clean/" #where to find the datas used
+    data_path_results = "/Users/maximecoppa/Desktop/Statapp/Datas_clean/" #where to create your new file
+
+    #2021
+    df_Données_Promos_2021 = file_to_dataframe(data_path + "df_Données_Promos_2021.csv",",")
+    df_odd = file_to_dataframe(data_path + "odd_2021.csv")
+    df_Données_Promos_2021_odd = join_dataFrames(df_Données_Promos_2021,df_odd[['CPROMO','TYPE_PROMON']] ,'CPROMO') #We create a new column 'TYPE_PROMON' on df_Données_Promos_2021
+
+    save_to_csv_file(df_Données_Promos_2021_odd,data_path_results + "df_Données_Promos_2021_odd.csv") #we save it on your Mac
+
+    #2022 
+    df_Données_Promos_2022 = file_to_dataframe(data_path + "df_Données_Promos_2022.csv",",")
+    df_odd = file_to_dataframe(data_path + "odd_2022.csv")
+    df_Données_Promos_2022_odd = join_dataFrames(df_Données_Promos_2022,df_odd[['CPROMO','TYPE_PROMON']] ,'CPROMO')
+
+    save_to_csv_file(df_Données_Promos_2022_odd,data_path_results + "df_Données_Promos_2022_odd.csv")
+
+    #2023
+    df_Données_Promos_2023 = file_to_dataframe(data_path + "df_Données_Promos_2023.csv",",")
+    df_odd = file_to_dataframe(data_path + "odd_2023.csv")
+    df_Données_Promos_2023_odd = join_dataFrames(df_Données_Promos_2023,df_odd[['CPROMO','TYPE_PROMON']] ,'CPROMO')
+
+    save_to_csv_file(df_Données_Promos_2023_odd,data_path_results + "df_Données_Promos_2023_odd.csv")
+
+    return True
+
+
+
+#2)
+#In this part we create the repartition of the reabos in function of some conditions 
+
+def repartition_reabo_cond():
+    """
+    This function is used to provide some statistics on the reabo habits
+    """
+
+    data_path = "/Users/maximecoppa/Desktop/Statapp/Datas_clean/" #where to find the datas used
+    data_path_results = "/Users/maximecoppa/Desktop/Statapp/Datas_comportement_reabo/" #where to create your new file
+
+    df_Données_Promos_2021_odd = file_to_dataframe(data_path + "df_Données_Promos_2021_odd.csv") #We open the df_Données_Promos_2021_odd where TYPEPROMO <-> CPROMO
+    df_Données_Reabos_2021 = file_to_dataframe(data_path + "df_Données_Reabos_2021.csv")
+    df_join = join_dataFrames(df_Données_Promos_2021_odd,df_Données_Reabos_2021,['ID_ABONNE','DATE_ACTE_REEL']) #We join the tables
+
+    df_join = df_mois_annee(df_join,'DATE_ACTE_REEL')
+
+    #We compute some statistcs using count_abo_conditions : this functions count the number of an occurence where the datas are group by conditions 
+    df_repartition_promo = count_abo_conditions(df_join,['TYPE_PROMON'],'ID_ABONNE')
+    save_to_csv_file(df_repartition_promo,data_path_results + "df_repartition_promo.csv")
+
+    df_type_promo_canaldistrib = count_abo_conditions(df_join,['TYPE_PROMON', 'CANAL_DISTRIB'],'ID_ABONNE')
+    save_to_csv_file(df_type_promo_canaldistrib,data_path_results + "type_promo_canaldistrib.csv")
+
+    df_month_canaldistrib = count_abo_conditions(df_join,['MONTH', 'YEAR', 'CANAL_DISTRIB'],'ID_ABONNE')
+    save_to_csv_file(df_month_canaldistrib,data_path_results + "month_canaldistrib.csv")
+
+    df_repartition_canaldistrib = count_abo_conditions(df_join,['TYPE_PROMON','MONTH', 'YEAR', 'CANAL_DISTRIB'],'ID_ABONNE')
+    save_to_csv_file(df_repartition_canaldistrib,data_path_results + "repartition_canaldistrib.csv")
+
+    df_repartition_region = count_abo_conditions(df_join,['TYPE_PROMON','MONTH','YEAR', 'REGION'],'ID_ABONNE')
+    save_to_csv_file(df_repartition_region,data_path_results + "repartition_region.csv")
+
+    df_repartition_secteur = count_abo_conditions(df_join,['TYPE_PROMON','MONTH','YEAR', 'SECTEUR'],'ID_ABONNE')
+    save_to_csv_file(df_repartition_secteur,data_path_results + "repartition_secteur.csv")
+
+    df_repartition_enseigne = count_abo_conditions(df_join,['TYPE_PROMON','MONTH','YEAR', 'ENSEIGNE'],'ID_ABONNE')
+    save_to_csv_file(df_repartition_enseigne,data_path_results + "repartition_enseigne.csv")
+
+    df_repartition_moypay = count_abo_conditions(df_join,['TYPE_PROMON','MONTH','YEAR', 'MOYEN_PAIEMENT'],'ID_ABONNE')
+    save_to_csv_file(df_repartition_moypay,data_path_results + "repartition_moypay.csv")
+
+    df_repartition_formule = count_abo_conditions(df_join,['TYPE_PROMON','MONTH','YEAR', 'FORMULE_PREC'],'ID_ABONNE')
+    save_to_csv_file(df_repartition_formule,data_path_results + "repartition_formule.csv")
+    
+    return True
+
+
+#3)
+#In this part we focus on the time of reabo between two abonnements
+
+def repartition_reabo():
+    """
+    This function is used to compute the time of reabo for each type
+    """
+
+    data_path = "/Users/maximecoppa/Desktop/Statapp/Datas_clean/" #where to find the datas used
+    data_path_results = "/Users/maximecoppa/Desktop/Statapp/Datas_comportement_reabo/" #where to create your new file
+
+    df_Données_Promos_2021 = file_to_dataframe(data_path + "df_Données_Promos_2021_odd.csv",",")
+    df_Données_Reabos_2021 = file_to_dataframe(data_path + "df_Données_Reabos_2021.csv",",")
+
+    end_abo = 'DATE_FIN_ABO_PREC'
+    date_reabo = 'DATE_ACTE_REEL'
+
+    df_Données_Reabos_2021 = time_reabo_columns(df_Données_Reabos_2021,end_abo,date_reabo) #Creation new colum "TIME_REABO"
+
+    df_Données_Promos_2021 = str_to_date(df_Données_Promos_2021,'DATE_ACTE_REEL') #Preparation join df_Données_Promos_2021 et df_Données_Reabos_2021
+    df_join = join_dataFrames(df_Données_Promos_2021,df_Données_Reabos_2021,['ID_ABONNE','DATE_ACTE_REEL']) #Join df_Données_Promos_2021 et df_Données_Reabos_2021
+
+    df_mean_time_reabo_promos = mean_time_reabo(df_join,'TYPE_PROMON','DELAI_REABO')  
+    save_to_csv_file(df_mean_time_reabo_promos,data_path_results + "df_mean_time_reabo_promos.csv") # Save on My Mac
+
+    df_join = join_dataFrames_outer(df_Données_Promos_2021,df_Données_Reabos_2021,['ID_ABONNE','DATE_ACTE_REEL']) 
+    df_mean_empty = mean_empty_col(df_join,'CPROMO','DELAI_REABO')
+    save_to_csv_file(df_mean_empty,data_path_results + "df_mean_empty.csv") # Save on My Mac
+
+
+    return True
+
+
+
+#4)
+#In this part we compute the number of time each person  abonned in one year
+
+def taux_consommation():
+
+    data_path = "/Users/maximecoppa/Desktop/Statapp/Datas_clean/" #where to find the datas used
+    data_path_results = "/Users/maximecoppa/Desktop/Statapp/Datas_comportement_reabo/" #where to create your new file
+
+    df_Données_Reabos_2021 = file_to_dataframe(data_path + "df_Données_Reabos_2021.csv",",")
+
+    df_nb_abos = count_abo_conditions(df_Données_Reabos_2021,['ID_ABONNE'],'ID_ABONNE')
+    df_nb_reabos_2021 = count_abo_conditions(df_nb_abos,['NB_ID_ABONNE'],'NB_ID_ABONNE')
+    df_nb_reabos_2021.rename(columns = {"NB_ID_ABONNE": "NB_ABONNEMENTS","NB_NB_ID_ABONNE": "NB_ID_ABONNE"},inplace=True) #Change name for a easier reading
+
+    save_to_csv_file(df_nb_reabos_2021, data_path_results + "df_nb_reabos_2021.csv")
+
+    return True
+
