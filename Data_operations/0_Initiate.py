8c66f472b5846f4156f03addb41904c503bbd711
TurboClem <127206798+TurboClem@users.noreply.github.com>
2023-12-25 11:37:15 +0100

    initiate the dfs

--- Data_operations/0_Initiate.py
+++ Data_operations/0_Initiate.py
@@ -1,9 +1,9 @@
-# Run pour clément
 import pandas as pd
 import sys 
 sys.path.append("Data_operations")
 from Tool_Functions.cleaning_data import *
 
+# Run pour clément
 data_path = "/Users/clementgadeau/Statapp/CSV files/"
 
 # Lancez ça pour télécharger les fichiers csv des df nettoyés, et des df concaténés.
---
+++ Data_operations/1_StatDescr.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import sys 
+sys.path.append("Data_operations")
+from Tool_Functions.cleaning_data import *
+
