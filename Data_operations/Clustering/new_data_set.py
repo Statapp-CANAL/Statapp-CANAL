cf0a77fdbaebeaea7b539e0fc6e633c1a9b6d32b
AntoineGilsonGH <antoine.gilson@icloud.com>
2024-03-03 17:13:56 +0100

    chef

--- Data_operations/Clustering/new_data_set.py
+++ Data_operations/Clustering/new_data_set.py
@@ -105,6 +105,26 @@ def create_new_data_set(data_path, data_path_results):
     return True
 
 
+def enlever_abos(data_path, data_path_results):
+    df = file_to_dataframe(data_path + "df_Données_Reabos_odd.csv")
+    df['NB_APPARITIONS'] = df.groupby('ID_ABONNE')['ID_ABONNE'].transform('count')
+    df_new = df[(df['NB_APPARITIONS'] < 34) & (df['NB_APPARITIONS'] > 2)]
+    save_to_csv_file(df_new, data_path_results + 'df_Données_Reabos_odd_new.csv')
+     
+    return True
+
+
+def ajouter_differences(df):
+    
+    promo_types = [col for col in df.columns if 'MEAN_TIME' in col and col != 'MOY_DELAI']
+    
+    for promo in promo_types:
+        new_col_name = promo + '_DIFF'
+        df[new_col_name] = df[promo] - df['MOY_DELAI']
+
+    return df
+
+
 def create_new_data_set_diff(data_path, data_path_results):
 
     df_Données_Reabos_odd = file_to_dataframe(data_path + "df_Donnees_Reabos_odd_new.csv")
@@ -119,8 +139,67 @@ def create_new_data_set_diff(data_path, data_path_results):
 
     return True
 
+def create_new_data_set_n_month(data_path, data_path_results):
+    """
+    Creates a new dataset 'new_data_set' from 'df_Données_Reabos_odd' dataframe where the columns are the 
+    TYPE_PROMON
+    """
 
+    # Load 'df_Données_Reabos_odd' dataframe
+    df = file_to_dataframe(data_path + "df_Donnees_Reabos_odd_new.csv")
+    df['DATE_ACTE_REEL'] = pd.to_datetime(df['DATE_ACTE_REEL'])
+    df['MONTH'] = df['DATE_ACTE_REEL'].dt.month
 
+    # Perform statistical analysis on 'df_Données_Reabos_odd' dataframe
+    df2 = count_abo_conditions(df, ['ID_ABONNE', 'MONTH'], 'DATE_ACTE_REEL')
 
+    # Create a pivot table from 'df' dataframe we exchange the lines and columns
+    new_data_set = df2.pivot_table(index='ID_ABONNE', columns='MONTH', values='NB_DATE_ACTE_REEL')
 
+    # Reset index of the pivot table
+    new_data_set = new_data_set.reset_index()
+
+    # Replace NaN values with 0
+    new_data_set.fillna(0, inplace=True)
+
+    # Convert floating-point values to integers
+    new_data_set = new_data_set.astype(int)
+
+    # Save 'new_data_set' dataframe to a CSV file
+    save_to_csv_file(new_data_set, data_path_results + "df_n_months_ODD.csv")
+
+    return True
         
+
+def create_new_data_set_n_month_pourc(data_path, data_path_results):
+    """
+    Creates a new dataset 'new_data_set' from 'df_Données_Reabos_odd' dataframe where the columns are the 
+    TYPE_PROMON
+    """
+
+    # Load 'df_Données_Reabos_odd' dataframe
+    df = file_to_dataframe(data_path + "df_Donnees_Reabos_odd_new.csv")
+    df['DATE_ACTE_REEL'] = pd.to_datetime(df['DATE_ACTE_REEL'])
+    df['MONTH'] = df['DATE_ACTE_REEL'].dt.month
+
+    # Perform statistical analysis on 'df_Données_Reabos_odd' dataframe
+    df2 = count_abo_conditions(df, ['ID_ABONNE', 'MONTH'], 'DATE_ACTE_REEL')
+
+    # Create a pivot table from 'df' dataframe we exchange the lines and columns
+    new_data_set = df2.pivot_table(index='ID_ABONNE', columns='MONTH', values='NB_DATE_ACTE_REEL')
+
+    new_data_set_pourc = new_data_set.apply(lambda x: (x / x.sum()) * 100, axis=1)
+
+    # Reset index of the pivot table
+    new_data_set_pourc = new_data_set_pourc.reset_index()
+
+    # Replace NaN values with 0
+    new_data_set_pourc.fillna(0, inplace=True)
+
+    # Convert floating-point values to integers
+    new_data_set_pourc = new_data_set_pourc.astype(int)
+
+    # Save 'new_data_set' dataframe to a CSV file
+    save_to_csv_file(new_data_set_pourc, data_path + "df_n_months_ODD_%.csv")
+
+    return True
