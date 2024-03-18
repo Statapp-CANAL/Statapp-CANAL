052a063631e9cbd8d901314315af686322f8a573
AntoineGilsonGH <antoine.gilson@icloud.com>
2024-03-18 11:11:35 +0100

    new datas diff outer

--- Data_operations/Clustering/new_data_set_outer.py
+++ Data_operations/Clustering/new_data_set_outer.py
@@ -43,3 +43,44 @@ def new_datas_join(data_path, data_path_results):
     df3 = join_dataFrames(df2, df, 'ID_ABONNE')
     save_to_csv_file(df3, data_path_results + 'new_datas_join_outer.csv')
     return True
+
+def ajouter_differences(df):
+    
+    promo_types = [col for col in df.columns if 'MEAN_TIME' in col and col != 'MOY_DELAI']
+    
+    for promo in promo_types:
+        new_col_name = promo + '_DIFF'
+        df[new_col_name] = df[promo] - df['MOY_DELAI']
+
+    return df
+
+def new_datas_diff_outer(data_path, data_path_results):
+
+   df = file_to_dataframe(data_path + 'new_datas_join_outer.csv')
+
+   columns_delai = [col for col in df.columns if col.endswith('TIME')]
+
+
+   df[columns_delai] = df[columns_delai].replace([np.inf, -np.inf], np.nan)
+
+
+   df['MOY_DELAI'] = df[columns_delai].mean(axis=1, skipna=True)
+
+   ajouter_differences(df)
+
+   df = df.drop(columns= ['Autres_MEAN_TIME', 'ODD 15 jours EV+_MEAN_TIME', 
+                             'ODD 15 jours TC_MEAN_TIME',
+                             'ODD 21 jours TC_MEAN_TIME', 'ODD 30 jours EV+_MEAN_TIME',
+                             'ODD 30 jours TC_MEAN_TIME','ODD 7 jours autre que SG_MEAN_TIME',
+                             'Semaine genéreuse_MEAN_TIME',
+                             'noODD_MEAN_TIME'])
+
+   df = df.drop(columns = ['Autres_n_REABOS', 'ODD 15 jours EV+_n_REABOS',
+                        'ODD 15 jours TC_n_REABOS',
+                        'ODD 21 jours TC_n_REABOS', 'ODD 30 jours EV+_n_REABOS',
+                        'ODD 30 jours TC_n_REABOS','ODD 7 jours autre que SG_n_REABOS',
+                          'Semaine genéreuse_n_REABOS', 'noODD_n_REABOS'])
+
+   save_to_csv_file(df, data_path_results + 'new_datas_diff_outer')
+
+   return True
