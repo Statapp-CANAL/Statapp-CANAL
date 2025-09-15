3a0a85a712d0ed0f734bd421e59e2f87fdc8dc11
MaximeCoppa <maxime.coppa@gmail.com>
2025-09-15 15:53:38 +0200

    name

--- Test/run.py
+++
@@ -1,6 +0,0 @@
-# %%  Run some test
-from data_operations.tool_functions.cleaning_data import *
-
-data_path = ""
-
-# %%
---
+++ test_generic/run.py
@@ -0,0 +1,6 @@
+# %%  Run some test
+from data_operation.tool_functions.cleaning_data import *
+
+data_path = ""
+
+# %%
